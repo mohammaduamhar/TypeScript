0387c76964d0bac0deacabbf4a25dc66c26d1dd8
mohammaduamhar <mamamhar254@gmail.com>
2024-06-14 07:43:29 +0530

    Debugging TypeScript Applications

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let age = 8;
+if (age < 10)
+    age += 10;
+console.log(age);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let age: number = 8;
+
+if(age <10)
+  age +=10;
+console.log(age)
